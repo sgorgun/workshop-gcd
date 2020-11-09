d77f60607ce52e4623a511080bbcaa8316763125
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.github.com>
2020-11-09 17:09:43 +0300

    initial-commit

---
+++ Gcd.1/Interfaces/Algorithm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Gcd
+{
+    internal abstract class Algorithm
+    {
+        public int Calculate(int first, int second)
+        {
+            throw  new NotImplementedException();
+        }
+
+        public int Calculate(int first, int second, out long milliseconds)
+        {
+            throw  new NotImplementedException();
+        }
+	
+        protected abstract int Func(int first, int second);
+    }
+}
